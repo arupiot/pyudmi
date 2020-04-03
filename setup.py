43bb7c10c57c63d9d424c4556e736e09b5361fa8
Paul Harter <paul@glowinthedark.co.uk>
2020-04-03 19:50:05 +0100

    fixing data files in package build

--- setup.py
+++ setup.py
@@ -2,15 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyudmi',
-    version='0.0.2',
+    version='0.0.3',
     url='https://github.com/arupiot/pyudmi.git',
     author='Paul Harter',
     author_email='paul@glowinthedark.co.uk',
     license="LICENSE",
     description='Helper classes for working with udmi',
     packages=find_packages('src'),
-    package_dir={'': 'src'},
-    package_data={'': ['*.json']},
-    include_package_data=True,
-    install_requires=['pytz', 'fastjsonschema'],
+    package_data={'udmi': ['schemata/daq/schemas/udmi/*.json']},
+    package_dir={'udmi': 'src/udmi'},
+    install_requires=['pytz', 'fastjsonschema']
 )
