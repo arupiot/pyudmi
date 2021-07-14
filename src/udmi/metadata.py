ccedfb25efc7098177c18db2a2dfe031cb5bde3a
Francesco Anselmo <francesco.anselmo@gmail.com>
2021-07-14 22:37:01 +0100

    cloud in metadata

--- src/udmi/metadata.py
+++ src/udmi/metadata.py
@@ -44,13 +44,14 @@ from .base import UDMIBase, DEFAULT_UDMI_VERSION
 class MetaData(UDMIBase):
 
     schema = "metadata.json"
-    __slots__ = ["version", "timestamp", "system", "hash", "gateway", "pointset"]
+    __slots__ = ["version", "timestamp", "system", "hash", "gateway", "pointset", "cloud"]
 
-    def __init__(self, timestamp, system, hash=None, gateway=None, pointset=None, version=DEFAULT_UDMI_VERSION):
+    def __init__(self, timestamp, system, hash=None, gateway=None, pointset=None, cloud=None, version=DEFAULT_UDMI_VERSION):
 
         self.timestamp = self.serialise_timestamp(timestamp)
         self.pointset = pointset
         self.gateway = gateway
         self.system = system
         self.hash = hash
+        self.cloud = cloud
         super().__init__(version)
